4978a0b09dec4b5b01ad052259ba7b9029ef1c36
TaeWoong Yu <ytw2854@gamil.com>
2026-08-30 15:49:54 +0900

    Create and Edit source

--- frontend/main.js
+++ frontend/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router/index.js';
-import store from './store/index.js';
+import router from './router';
+import store from './store';
 
-const app = createApp(App);
-app.use(router);
-app.use(store);
-app.mount('#app');
+createApp(App)
+  .use(router)
+  .use(store)
+  .mount('#app');
